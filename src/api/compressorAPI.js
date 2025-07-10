1f1f81c54723e49495f892e7abbde91d8bbc9197
nemish bhuva <nemishbhuva01@gmail.com>
2025-07-10 21:53:15 +0530

    minor change

--- src/api/compressorAPI.js
+++ src/api/compressorAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Get the API URL from environment variables, with a fallback for development
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 // Create a configured Axios instance
 const api = axios.create({
